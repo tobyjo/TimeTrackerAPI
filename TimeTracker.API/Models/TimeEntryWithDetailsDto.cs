3f9abdfb10a1fd4d023662bb73c732f9f7d1feca
Toby Jones <toby1408@gmail.com>
2025-10-10 15:02:41 +0100

    Enabled API to be connected to via IP address of PC rather than just localhost

--- TimeTracker.API/Models/TimeEntryWithDetailsDto.cs
+++ TimeTracker.API/Models/TimeEntryWithDetailsDto.cs
@@ -7,9 +7,13 @@ namespace TimeTracker.API.Models
         public DateTime StartDateTime { get; set; }
         public DateTime EndDateTime { get; set; }
         public string ProjectCode { get; set; } = string.Empty;
+
+        public int ProjectID { get; set; }
         public int UserID { get; set; }
         public string ProjectDescription { get; set; } = string.Empty;
 
         public string SegmentTypeName { get; set; } = string.Empty;
+
+        public int SegmentTypeID { get; set; }
     }
 }
